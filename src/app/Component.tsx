7991ad095b1e2e0553afd0aa995dc962ff420cfb
nayeon <nyeonk@devcra.com>
2024-10-01 19:14:01 +0900

    [ feat ] 여러 행에서의 drag and drop 구현

--- src/app/Component.tsx
+++ src/app/Component.tsx
@@ -3,21 +3,36 @@
 import React, { useState } from "react";
 
 const DATA = [
-  { id: 1, name: "코리", seq: 1 },
-  { id: 2, name: "마리", seq: 3 },
+  {
+    id: 1,
+    list: [
+      { id: 1, name: "코리", seq: 1 },
+      { id: 2, name: "마리", seq: 3 },
+    ],
+  },
+  {
+    id: 2,
+    list: [
+      { id: 3, name: "아아", seq: 1 },
+      { id: 4, name: "우우", seq: 3 },
+      { id: 5, name: "오오", seq: 5 },
+    ],
+  },
 ];
 
 export default function DraggableComponent() {
   const [data, setData] = useState(DATA);
-  const [dropzoneIndex, setDropzoneIndex] = useState<number | null>(null); // 드롭존의 index 값 (드롭 영역 구분을 위해 사용)
-  const [draggedId, setDraggedId] = useState<number | null>(null); // 드래그 중인 요소의 Id 값
+  const [dropzoneIndex, setDropzoneIndex] = useState<boolean[][]>([[], []]);
+  const [draggedId, setDraggedId] = useState<number | null>(null);
+  const [currentRow, setCurrentRow] = useState<number | null>(null);
 
-  const handleDragStart = (id: number) => {
+  const handleDragStart = (id: number, row: number) => {
     setDraggedId(id);
+    setCurrentRow(row);
   };
 
   const handleDragEnd = () => {
-    setDropzoneIndex(null);
+    setDropzoneIndex([[], []]);
     setDraggedId(null);
   };
 
@@ -25,64 +40,89 @@ export default function DraggableComponent() {
     event.preventDefault();
   };
 
-  const handleDragEnter = (index: number) => {
-    setDropzoneIndex(index);
+  const handleDragEnter = (row: number, index: number) => {
+    if (currentRow === row) {
+      setDropzoneIndex((prev) => {
+        const updatedDropzoneState = [...prev];
+        updatedDropzoneState[row][index] = true;
+        return updatedDropzoneState;
+      });
+    }
   };
 
-  const handleDragLeave = () => {
-    setDropzoneIndex(null);
+  const handleDragLeave = (row: number, index: number) => {
+    setDropzoneIndex((prev) => {
+      const updatedDropzoneState = [...prev];
+      updatedDropzoneState[row][index] = false;
+      return updatedDropzoneState;
+    });
   };
 
-  const handleDrop = (event: React.DragEvent<HTMLElement>, index: number) => {
-    if (draggedId === null) {
+  const handleDrop = (
+    event: React.DragEvent<HTMLElement>,
+    row: number,
+    index: number
+  ) => {
+    if (draggedId === null || currentRow !== row) {
       return;
     }
     event.preventDefault();
 
-    const isExist = data.some((item) => item.seq === index + 1);
+    const isExist = data[row].list.some((item) => item.seq === index + 1);
+
     if (isExist) {
       return;
     } else {
       setData((prev) =>
-        prev.map((item) =>
-          item.id === draggedId ? { ...item, seq: index + 1 } : item
-        )
+        prev.map((group, gIdx) => {
+          if (gIdx === row) {
+            return {
+              ...group,
+              list: group.list.map((item) =>
+                item.id === draggedId ? { ...item, seq: index + 1 } : item
+              ),
+            };
+          }
+          return group;
+        })
       );
     }
 
-    setDropzoneIndex(null);
+    setDropzoneIndex([[], []]);
     setDraggedId(null);
   };
 
   return (
-    <div className="w-full h-40 flex gap-2 py-2">
-      {Array.from({ length: 4 }).map((_, i) => (
-        <div
-          key={i}
-          onDragOver={handleDragOver}
-          onDragEnter={() => handleDragEnter(i)}
-          onDragLeave={handleDragLeave}
-          onDrop={(e) => handleDrop(e, i)}
-          className={`w-full h-full bg-gray-300 border-x flex items-center justify-center ${
-            dropzoneIndex === i ? "bg-gray-200" : ""
-          }`}
-        >
-          {data
-            .filter((data) => data.seq == i + 1)
-            .map((e) => (
-              <div
-                key={e.id}
-                draggable
-                onDrag={handleDragOver}
-                onDragStart={() => handleDragStart(e.id)}
-                onDragEnd={handleDragEnd}
-                className="w-full h-full z-10 bg-pink-300 cursor-pointer flex justify-center items-center"
-              >
-                {e.name}
-              </div>
-            ))}
-        </div>
-      ))}
+    <div className="w-full h-full grid grid-cols-5">
+      {data.map((el, rowIdx) =>
+        Array.from({ length: 5 }).map((_, colIdx) => (
+          <div key={rowIdx + colIdx} className="h-40 flex gap-2 py-2">
+            <div
+              onDragOver={handleDragOver}
+              onDragEnter={() => handleDragEnter(rowIdx, colIdx)}
+              onDragLeave={() => handleDragLeave(rowIdx, colIdx)}
+              onDrop={(e) => handleDrop(e, rowIdx, colIdx)}
+              className={`w-full h-full bg-gray-300 border-x flex items-center justify-center ${
+                dropzoneIndex[rowIdx][colIdx] ? "bg-gray-200" : ""
+              }`}
+            >
+              {el.list
+                .filter((item) => item.seq === colIdx + 1)
+                .map((e) => (
+                  <div
+                    key={e.id}
+                    draggable
+                    onDragStart={() => handleDragStart(e.id, rowIdx)}
+                    onDragEnd={handleDragEnd}
+                    className="w-full h-full z-10 bg-pink-300 cursor-pointer flex justify-center items-center"
+                  >
+                    {e.name}
+                  </div>
+                ))}
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 }
